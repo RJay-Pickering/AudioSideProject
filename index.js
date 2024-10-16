7bd8799ca59a41cb4fe4792c854c17c5e92e1eee
RJay-Pickering <rjaypickering@gmail.com>
2024-10-16 14:34:23 -0500

    Trying to fix audio for modile users

--- index.js
+++ index.js
@@ -15,4 +15,29 @@ function audioCue() {
 }
 
 // document.addEventListener("click", audioCue)
-audioCue()
+// audioCue()
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+if (isMobile.any() !== null) {
+    document.addEventListener("click", audioCue)
+} else {
+    audioCue()
+}
